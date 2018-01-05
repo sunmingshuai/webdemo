d5add90b698c6de3643e9548678202e40b825ad5
Sunms <1106505519@qq.com>
2018-01-05 17:21:58 +0800

    first commit

--- src/main/java/com/github/service/impl/TransferServiceImpl.java
+++ src/main/java/com/github/service/impl/TransferServiceImpl.java
@@ -26,37 +26,48 @@ public class TransferServiceImpl implements TransferService {
 //		return true;
 
 //		return moneyInAndOut(in,out,amount);
-
-		return this.moneyInAndOut(in,out,amount);
+		boolean transfer = moneyInAndOut(in,out,amount);
+		int i = 1/0;
+		return this.moneyInAndOut(in, out, amount);
 
 	}
 
 	/**
 	 * 1. transfer(有事务)调用moneyInAndOut(没事务) 事务起作用
 	 * 2. transfer(没事务)调用moneyInAndOut(有事务) 即AOP方法内调用 事务不起作用
+	 */
+//	@Transactional
+	public boolean moneyInAndOut(String in, String out, float amount) {
+		userMapper.updateUserBalance(UserMapper.IN, in, 100f);
+		int i = 1 / 0;
+		userMapper.updateUserBalance(UserMapper.OUT, out, 100f);
+		return true;
+	}
+
+	/**
+	 * 与moneyInAndOut方法一起测试两个事务的执行
 	 * @param in
 	 * @param out
 	 * @param amount
 	 * @return
 	 */
-//	@Transactional
-	public boolean moneyInAndOut(String in, String out, float amount){
-		userMapper.updateUserBalance(UserMapper.IN,in,100f);
+	@Transactional
+	public boolean moneyTransferInAndOut(String in, String out, float amount) {
+		userMapper.updateUserBalance(UserMapper.IN, in, 100f);
 		int i = 1 / 0;
-		userMapper.updateUserBalance(UserMapper.OUT,out,100f);
+		userMapper.updateUserBalance(UserMapper.OUT, out, 100f);
 		return true;
 	}
+
 	/**
 	 * 测试方法调用
-	 * @param in
-	 * @param amount
 	 */
-	public void moneyIn(String in,float amount){
-		userMapper.updateUserBalance(UserMapper.IN,in,100f);
+	public void moneyIn(String in, float amount) {
+		userMapper.updateUserBalance(UserMapper.IN, in, 100f);
 	}
 
-	public void moneyOut(String out,float amount){
-		userMapper.updateUserBalance(UserMapper.OUT,out,100f);
+	public void moneyOut(String out, float amount) {
+		userMapper.updateUserBalance(UserMapper.OUT, out, 100f);
 	}
 
 }
