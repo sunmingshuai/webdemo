2ffcf85a0c2c3943080d46474556ec8753fb18f7
Sunms <1106505519@qq.com>
2018-02-01 21:04:08 +0800

    first commit

--- src/test/java/com/github/UserMapperTest.java
+++ src/test/java/com/github/UserMapperTest.java
@@ -1,16 +1,17 @@
 package com.github;
 
-import com.github.domain.User;
-import com.github.mapper.UserMapper;
-
-import junit.framework.TestCase;
+import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
+import com.github.domain.User;
+import com.github.mapper.UserMapper;
+
+
+import junit.framework.TestCase;
 
 @ContextConfiguration("classpath:spring/*.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,6 +23,14 @@ public class UserMapperTest extends TestCase {
 	@Test
 	public void testSelectUserById() {
 		User user = userMapper.selectUserById(1);
+		user.setDesc("扎心了吖！\uD83D\uDE02感觉都是命吖！命吖！");
 		System.out.println(user);
+	}
+
+	@Test
+	public void updateUserTest(){
+		User user = userMapper.selectUserById(1);
+		user.setDesc("扎心了吖！感觉都是命吖！命吖！");
+		userMapper.updateUser(user);
 	}
 }
